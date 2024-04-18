62442f25c327f89011ad107de1a81b5a1948078f
LQ <143152892+LQ458@users.noreply.github.com>
2024-04-18 20:02:24 +0800

    Create token.js

---
+++ models/token.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const tokenSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    token: {
+        type: String,
+        required: true,
+    },
+    createdAt: { 
+        type: Date, 
+        expires: 60, // This token will expire in 1 hour
+        default: Date.now 
+    }
+})
+
+const Token = mongoose.models.Token || mongoose.model("Token", tokenSchema);
+export default Token;
